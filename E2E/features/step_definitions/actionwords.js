8a234b638427437ba02dbb4c2f91ddf16e9491ab
jerp <info@jacobo.tech>
2017-12-01 12:46:34 +0000

    clean up code climate smells

--- E2E/features/step_definitions/actionwords.js
+++ E2E/features/step_definitions/actionwords.js
@@ -1,5 +1,16 @@
 var assert = require('cucumber-assert');
 
+function GetElement(id) {
+    return this.driver.findElement({
+        id: id
+    }).getText();
+}
+
+function ClickElement(id) {
+    return this.driver.findElement({
+        id: id
+    }).click()
+}
 
 exports.Actionwords = {
     iAmAbleToCall: function() {
@@ -7,47 +18,35 @@ exports.Actionwords = {
     },
     user1IsAbleToCallUser2: function(user1, user2, callback) { callback(); },
     theCallP1ButtonIsClicked: function(p1, callback) {
-        this.driver.findElement({
-            id: "call-" + p1
-        }).click().then(callback);
+        return ClickElement.call(this, "call-" + p1).then(callback);
     },
 
     p1ShouldRecieveACall: function(p1, callback) {
-        this.driver.findElement({
-            id: p1 + "-state"
-        }).getText().then(function(text) {
+        GetElement.call(this, p1 + "-state").then(function(text) {
             assert.equal(text, "Recieving call", p1 + " did not recieve call");
-        }).then(callback).catch(function(err) {
-            console.log("something ain't right", err);
-            callback()
-        });
+        }).then(callback);
     },
     p1ShouldReceiveACall: function(p1) {
         console.log("ya dun kno")
     },
-    
-    p1IsReceivingAPhoneCall: function(p1,callback) {
-          var world = this;
-        this.driver.findElement({
-            id: p1 + "-state"
-        }).getText().then(function(userText) {
+
+    p1IsReceivingAPhoneCall: function(p1, callback) {
+        var world = this;
+        GetElement.call(this, p1 + "-state").then(function(userText) {
             assert.equal(userText, "Recieving call", p1 + " should be recieving phone call ");
-        }).then(callback).catch(function(err) {
-            console.log("something ain't right", err);
-            callback()
-        });;
+        }).then(callback)
     },
-    p2AcceptsTheCall: function(p1,callback) {
-      callback(); 
+    p2AcceptsTheCall: function(p1, callback) {
+        callback();
     },
     p1AndP2AreConnected: function(p1, p2, callback) {
-      callback(); 
+        callback();
     },
     theUserRejectsTheCall: function() {
         console.log("ya dun kno")
     },
-    p1IsInformedofP2: function(p1,p2, callback) {
+    p1IsInformedofP2: function(p1, p2, callback) {
         console.log("ya dun kno")
-        callback(); 
+        callback();
     }
 };
--- e2e.sh
+++
@@ -1,3 +0,0 @@
-if[-x E2E/testproject/src/scripts/dist] then 
-    rm -rf E2E/testproject/src/scripts/dist 
-fi 
