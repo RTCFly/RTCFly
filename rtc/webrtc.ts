72b2139eff308e4d528500806fd3eed5444af747
jerp <info@jacobo.tech>
2018-03-06 12:23:03 +0000

    add basic webrtc initialisation

--- rtc/webrtc.ts
+++ rtc/webrtc.ts
@@ -19,26 +19,41 @@ export default class WebRTC extends EventEmitter implements IRTCService {
     init(config: IRTCConfiguration){
         this._config = config;
     }
-    initSession(params:ICallParams){ 
-        this._mediaWrapper.setVideoElements({
-            localElement:params.localElement,
-            remoteElement:params.remoteElement
-        });
-        if(params.audio || params.video){
-            this._flyAdapter.getUserMedia({
-                audio:params.audio || false, 
-                video:params.video || false
-            }).then((localStream => 
-                this._videoWrapper.setVideoStream({
-                    localStream
-                }));
-        } else {
+    /**
+     * Initialise the local call session  
+     * 
+     */
+    public initSession(params:ICallParams):Promise<string>{ 
+        return new Promise((resolve, reject) => {
             
-        }
+            this._mediaWrapper.setVideoElements({
+                localElement:params.localElement,
+                remoteElement:params.remoteElement
+            });
+            
+            if(params.audio || params.video){
+                this._flyAdapter.getUserMedia({
+                    audio:params.audio || false, 
+                    video:params.video || false
+                }).then((localStream => 
+                    this._videoWrapper.setVideoStream({
+                        localStream
+                    })).then(()=>{
+                        this.initPeerConnection(params, resolve, localStream);
+                    });
+            } else {
+                this.initPeerConnection(params, resolve)
+            }
+        });
+        
     }
-    private initPeerConnection(params:ICallParams, stream?:IMediaStream){
+    private initPeerConnection(params:ICallParams, resolve:Promise<string>, stream?:IMediaStream){
         this._peerConnection = new this._FlyAdapter.RTCPeerConnection(this._config);
+        if(stream){
+            this._peerConnection.addStream(stream);
+        }
         this._registerPeerConnectionCallbacks();
+        this._setupLocalSession(params, resolve);
     }
     private registerPeerConnectionCallbacks(params:ICallParams){
         this._peerConnection.onicecandidate = event => 
@@ -48,11 +63,18 @@ export default class WebRTC extends EventEmitter implements IRTCService {
             this._mediaWrapper.setVideoStream({
                 remoteStream
             });
-            
-    
-        
     }
-    getDevices():Promise<Array<IMediaDevice>>{
+    
+    private setupLocalSession(params:ICallParams, resolve:Promise<string>){
+        if(params.caller){
+            this._peerConnection.createOffer().then(offer =>{
+                this._peerConnection.setLocalDescription(offer);
+                resolve(offer);
+            });
+        }
+    }
+    
+    public getDevices():Promise<Array<IMediaDevice>>{
         return this._flyAdapter.enumerateDevices().then((devices:any) =>{
             
         });
